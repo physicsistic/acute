c75cc45e42a58b2df66f4462d2bb2abb3180ba4e
Aza <azaaza@gmail.com>
2013-07-16 23:39:59 -0700

    - Buttons are now smarter. They can be active or inactive. They can be faded in.

--- utils.lua
+++ utils.lua
@@ -2,19 +2,45 @@ local storyboard = require "storyboard"
 local M = {}
 
 function M.createButton(label, x, y, width, height)
+	local defaultButtonHeight = display.contentHeight / 10
+	local defaultButtonWidth = display.contentWidth * 3/4
+
+	if width == nil  then width  = defaultButtonWidth end
+	if height == nil then height = defaultButtonHeight end 
+
+
 	local button = display.newGroup()
 	button.x = x
 	button.y = y
 	local buttonBackground = display.newRect(0, 0, width, height)
-	buttonBackground:setFillColor(46, 204, 113)
+	
 	buttonBackground:setStrokeColor(236, 240, 241)
 	buttonBackground.strokeWidth = 1
 	local buttonLabel = display.newText(label, 0, 0, storyboard.states.font.bold, 20)
 	button:insert(buttonLabel, true)
 	button:insert(1, buttonBackground, true)
 	button.bg = buttonBackground
+
+	function button.setActive( isActive )
+		if isActive then
+			buttonBackground:setFillColor(46, 204, 113)
+		else
+			buttonBackground:setFillColor(200,200,200)
+		end
+	end
+
+	function button.fadeIn(time)
+		button.alpha = 0
+		if time == nil then time = 500 end
+		transition.to( button, {time = time, alpha = 1} )
+	end
+
+	button.setActive( true )
+
 	return button
 end
+
+M.buttonSep = display.contentHeight / 20
 
 -- A general function for dragging physics bodies
 function M.dragBody( event )
